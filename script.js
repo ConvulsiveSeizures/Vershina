185e6050f24fe12bdc3a678a98bc10a3b38e85c5
Seizure <78146652+ConvulsiveSeizure@users.noreply.github.com>
2021-03-10 22:25:55 +0300

    add category & categoryGallery

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
 $(document).ready(() => {
+
+
+    //toggle theme
    $("#btnToggleTheme").on("click", () => {
     $("#themeWrapper").toggleClass("changeTheme")
     $(".tg-color").toggleClass("white")
     $("#sunIcon").toggleClass("goHidden")
+    $(".stars").toggleClass("themeTrue")
    })
     setTimeout(() => {
         $("#themeWrapper").addClass("changeTheme")
         $("#timeBlur").addClass("active")
-    }, 500)
+    }, 100)
+
+    $(".slide").addClass("dis")
+    $("#linkCont div").addClass("dis")
+
+
+    //button Point onCLick
+    $("#pointContainer a").on("click", (e) => {
+        $("body").css("overflowY", "hidden")
+        let curId = e.target.dataset["id"]
+        //set z-index
+        $("#mapWrapper").addClass("toview")
+        $("#themeWrapper").addClass("timeBlur")
+        $(`.mapContainer[data-id=${curId}]`).addClass("active")
+    })
+
+    //close map
+    $("#mapWrapper button").on("click", (e) => {
+        $("body").css("overflowY", "scroll")
+        $("#mapWrapper").removeClass("toview")
+        $("#themeWrapper").removeClass("timeBlur")
+        $(".mapContainer").removeClass("active")
+    })
+
+
+    //open category => tours
+    $(".categoryBtn").on("click", () => {
+        if (!$("#themeWrapper").hasClass("changeTheme")) {
+            $("#galleryWrapper").addClass("tgColor")
+        }
+        $("body").css("overflowY", "hidden")
+        $("#themeWrapper").addClass("timeBlur")
+        $("#gallerySection").addClass("frontview")
+    })
+
+    //close tours
+    $("#closeGalleryBtn").on("click", (e) => {
+        $("body").css("overflowY", "scroll")
+        $("#gallerySection").removeClass("frontview")
+    })
 })
+
